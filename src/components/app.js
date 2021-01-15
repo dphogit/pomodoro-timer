9cf02e8bb33e89556f84055c94fa55b367f72f2e
Dean Phommahaxay <deanp21@gmail.com>
2021-01-15 21:01:34 +1300

    Added styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from "react";
+
 import Timer from "./timer";
+import Footer from "./footer";
 
 const App = () => {
   return (
     <div className="App">
       <Timer />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+// Credentials
+
+const Footer = () => {
+  return (
+    <footer>
+      Created by{" "}
+      <a href="https://github.com/dphogit" target="_blank" rel="noreferrer">
+        dphogit
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
